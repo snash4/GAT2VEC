28638485f64b34e9e1b647f146b108303bfdaf98
Özlem Muslu <ozlemmuslu@gmail.com>
2018-07-18 16:24:50 +0200

    Add AUC score and make the code more Pythonic

--- Evaluation/Classification.py
+++ Evaluation/Classification.py
@@ -1,41 +1,31 @@
 import pandas as pd
 import numpy as np
 import csv
-import random, os
-from sklearn import svm
-from sklearn.metrics import f1_score, accuracy_score
+import os
+from sklearn.metrics import f1_score, accuracy_score, roc_auc_score
 from sklearn.multiclass import OneVsRestClassifier
-from sklearn.model_selection import train_test_split
 from sklearn.model_selection import ShuffleSplit
 from sklearn import linear_model
 from sklearn import preprocessing
 
-''' This class performs multi-class/multi-label classification tasks'''
 
 class Classification:
+    """This class performs multi-class/multi-label classification tasks."""
 
     def __init__(self, dataset, multilabel=False):
         self.dataset = dataset
-        self.output = {"DATASET": [], "TR": [], "accuracy": [], "f1micro": [], "f1macro": []}
+        self.output = {"TR": [], "accuracy": [], "f1micro": [], "f1macro": [], "auc": []}
         self.dataset_dir = os.getcwd() + "/data/" + dataset + "/"
         self.multi_label = multilabel
         if self.multi_label:
-            self.labels = self.getMultiLabels()
+            self.labels = self.get_multilabels()
         else:
-            self.labels = self.getLabels()
+            self.labels = self.get_labels()
 
-    """ returns the embeddings read from file fname"""
-    def get_embeddingDF(self, fname):
-        df = pd.read_csv(fname, header=None, skiprows=1, delimiter=' ')
-        df.sort_values(by=[0], inplace=True)
-        # dfs = dfs[:num_nodes]
-        df = df.set_index(0)
-        return df.as_matrix(columns=df.columns[0:])
-
-    ''' returns list of labels ordered by the node id's '''
-    def getLabels(self):
+    def get_labels(self):
+        """ returns list of labels ordered by the node id's """
         lblmap = {}
-        fname = self.dataset_dir+'labels_maped.txt'
+        fname = self.dataset_dir + 'labels_maped.txt'
         with open(fname, 'r') as freader:
             lines = csv.reader(freader, delimiter='\t')
             for row in lines:
@@ -46,8 +36,8 @@ class Classification:
         labels = [lblmap[vid] for vid in node_list]
         return np.array(labels)
 
-    ''' returns the multibinarized object for multilabel datasets'''
-    def getMultiLabels(self,delim='\t'):
+    def get_multilabels(self, delim='\t'):
+        """ returns the multibinarized object for multilabel datasets."""
         lblmap = {}
         fname = self.dataset_dir + 'labels_maped.txt'
         with open(fname, 'r') as freader:
@@ -60,10 +50,10 @@ class Classification:
         nlist = lblmap.keys()
         nlist.sort()
         labels = [lblmap[vid] for vid in nlist]
-        return self.binarizelabels(labels)
+        return self.binarize_labels(labels)
 
-    ''' returns the multilabelbinarizer object'''
-    def binarizelabels(self, labels, nclasses=None):
+    def binarize_labels(self, labels, nclasses=None):
+        """ returns the multilabelbinarizer object"""
         if nclasses == None:
             mlb = preprocessing.MultiLabelBinarizer()
             return mlb.fit_transform(labels)
@@ -71,15 +61,69 @@ class Classification:
         mlb = preprocessing.MultiLabelBinarizer(classes=range(nclasses))
         return mlb.fit_transform(labels)
 
-    ''' returns the classifier'''
-    def getclassifier(self):
+    def evaluate(self, model, label=False):
+        embedding = 0
+        if not label:
+            embedding = self.get_embeddingDF(model)
+
+        clf = self.get_classifier()
+        TR = [0.1, 0.3, 0.5]  # the training ratio for classifier
+        for tr in TR:
+            print("TR ... ", tr)
+            if label:
+                model = "./embeddings/" + self.dataset + "_gat2vec_label_" + str(
+                    int(tr * 100)) + ".emb"
+                if isinstance(model, str):
+                    embedding = self.get_embeddingDF(model)
+
+            self.evaluate_tr(clf, embedding, tr)
+        print("Training Finished")
+
+        df = pd.DataFrame(self.output)
+        return df.groupby(axis=0, by="TR").mean()
+
+    def get_embeddingDF(self, fname):
+        """returns the embeddings read from file fname."""
+        df = pd.read_csv(fname, header=None, skiprows=1, delimiter=' ')
+        df.sort_values(by=[0], inplace=True)
+        df = df.set_index(0)
+        return df.as_matrix(columns=df.columns[0:])
+
+    def get_classifier(self):
+        """ returns the classifier"""
         log_reg = linear_model.LogisticRegression(n_jobs=8)
         ors = OneVsRestClassifier(log_reg)
         return ors
 
-    ''' predicts and returns the top k labels for multi-label classification
-    k depends on the number of labels in y_test'''
+    def evaluate_tr(self, clf, embedding, tr):
+        """ evaluates an embedding for classification on training ration of tr."""
+        ss = ShuffleSplit(n_splits=10, train_size=tr, random_state=2)
+        for train_idx, test_idx in ss.split(self.labels):
+            X_train, X_test, Y_train, Y_test = self._get_split(embedding, test_idx, train_idx)
+            pred, probs = self.get_predictions(clf, X_train, X_test, Y_train, Y_test)
+            self.output["TR"].append(tr)
+            self.output["accuracy"].append(accuracy_score(Y_test, pred))
+            self.output["f1micro"].append(f1_score(Y_test, pred, average='micro'))
+            self.output["f1macro"].append(f1_score(Y_test, pred, average='macro'))
+            if len(np.unique(self.labels)) == 2:
+                self.output["auc"].append(roc_auc_score(Y_test, probs[:, 1]))
+            else:
+                self.output["auc"].append(0)
+
+    def _get_split(self, embedding, test_id, train_id):
+        return embedding[train_id], embedding[test_id], self.labels[train_id], self.labels[test_id]
+
+    def get_predictions(self, clf, X_train, X_test, Y_train, Y_test):
+        if self.multi_label:
+            return self.fit_and_predict_multilabel(clf, X_train, X_test, Y_train, Y_test)
+        else:
+            clf.fit(X_train, Y_train)  # for multi-class classification
+            # TODO: return values of multi-label and binary label aren't the same anymore.
+            return clf.predict(X_test), clf.predict_proba(X_test)
+
     def fit_and_predict_multilabel(self, clf, X_train, X_test, y_train, y_test):
+        """ predicts and returns the top k labels for multi-label classification
+        k depends on the number of labels in y_test."""
         clf.fit(X_train, y_train)
         y_pred_probs = clf.predict_proba(X_test)
 
@@ -91,74 +135,4 @@ class Classification:
             probs_ = y_pred_probs[i, :]
             labels_ = tuple(np.argsort(probs_).tolist()[-k:])
             pred_labels.append(labels_)
-        return self. binarizelabels(pred_labels, nclasses)
-
-    def getPredictions(self, clf, X_train, X_test, Y_train, Y_test):
-        if self.multi_label:
-            return self.fit_and_predict_multilabel(clf, X_train, X_test, Y_train, Y_test)
-        else:
-            clf.fit(X_train, Y_train) #for multi-class classification
-            return clf.predict(X_test)
-
-    def _get_accuracy(self, tlabels, plabels):
-        return accuracy_score(tlabels, plabels)
-
-    def _get_f1micro(self, tlabels, plabels):
-        return f1_score(tlabels, plabels, average='micro')
-
-    def getF1macro(self, tlabels, plabels):
-        return f1_score(tlabels, plabels, average='macro')
-
-    def _add_rows(self, data, output, tr, acc, f1micro, f1macro):
-        output['DATASET'].append(data)
-        output["TR"].append(tr)
-        output["accuracy"].append(np.mean(np.array(acc)))
-        output["f1micro"].append(np.mean(np.array(f1micro)))
-        output["f1macro"].append(np.mean(np.array(f1macro)))
-
-    ''' evaluates an embedding for classification on training ration of tr'''
-    def evaluate_tr(self, clf, embedding, tr):
-        num_nodes = self.labels.size
-        ss = ShuffleSplit(n_splits=10, train_size=tr, random_state=2)
-        gat2vecAcc = []
-        gat2vecF1macro = []
-        gat2vecF1micro = []
-        for train_idx, test_idx in ss.split(self.labels):
-            X_train, X_test, Y_train, Y_test = embedding[train_idx], embedding[test_idx], \
-                                               self.labels[train_idx], self.labels[test_idx]
-            pred = self.getPredictions(clf, X_train, X_test, Y_train, Y_test)
-            gat2vecAcc.append(self._get_accuracy(Y_test, pred))
-            gat2vecF1micro.append(self._get_f1micro(Y_test, pred))
-            gat2vecF1macro.append(self.getF1macro(Y_test, pred))
-
-        # self.addRows(self.dataset, self.output, tr, gat2vecAcc, gat2vecF1micro, gat2vecF1macro)
-        # outDf = pd.DataFrame(self.output)
-        return gat2vecAcc, gat2vecF1micro, gat2vecF1macro
-
-    def evaluate(self, model, label = False):
-        output = {"DATASET": [], "TR": [], "accuracy": [], "f1micro": [], "f1macro":[]}
-        embedding = 0
-        if label == False:
-            if isinstance(model, str):
-                embedding = self.get_embeddingDF(model)
-            else:
-                embedding = self.get_embeddingDF(model)
-
-        clf = self.getclassifier()
-        TR = [0.1, 0.3, 0.5] # the training ratio for classifier
-        for tr in TR:
-            print "TR ... ", tr
-            if label == True:
-                model = "./embeddings/" + self.dataset + "_gat2vec_label_" + str(int(tr * 100)) + ".emb"
-                if isinstance(model, str):
-                    embedding = self.get_embeddingDF(model)
-
-            gat2vecAcc, gat2vecF1micro, gat2vecF1macro = self.evaluate_tr(clf, embedding, tr)
-            self._add_rows(self.dataset, output, tr, gat2vecAcc, gat2vecF1micro, gat2vecF1macro)
-        print "SVC Training Finished"
-
-        print "results"
-        outDf = pd.DataFrame(output)
-        return outDf
-
-
+        return self.binarize_labels(pred_labels, nclasses)
