5464ca6747acf1a2043827cdd88ef463b89211f1
Özlem Muslu <ozlemmuslu@gmail.com>
2018-07-30 16:41:38 +0200

    Redo: Perform repeated stratified cross validation, if that is selected over ShuffleSplit with given training ratios
    This time it supports missing node labels

--- src/evaluation/classification.py
+++ src/evaluation/classification.py
@@ -1,10 +1,11 @@
+from collections import defaultdict
+
 import pandas as pd
 import numpy as np
 import csv
-import os
 from sklearn.metrics import f1_score, accuracy_score, roc_auc_score
 from sklearn.multiclass import OneVsRestClassifier
-from sklearn.model_selection import ShuffleSplit
+from sklearn.model_selection import ShuffleSplit, StratifiedKFold
 from sklearn import linear_model
 from sklearn import preprocessing
 from src import paths
@@ -13,16 +14,16 @@ from src import paths
 class Classification:
     """This class performs multi-class/multi-label classification tasks."""
 
-    def __init__(self, dataset, multilabel=False, tr=[0.1, 0.3, 0.5]):
+    def __init__(self, dataset, tr, multilabel=False):
         self.dataset = dataset
         self.output = {"TR": [], "accuracy": [], "f1micro": [], "f1macro": [], "auc": []}
         self.TR = tr  # the training ratio for classifier
         self.dataset_dir = paths.get_dataset_dir(dataset)
         self.multi_label = multilabel
         if self.multi_label:
-            self.labels, self.label_count = self.get_multilabels()
+            self.labels, self.label_ind, self.label_count = self.get_multilabels()
         else:
-            self.labels, self.label_count = self.get_labels()
+            self.labels, self.label_ind, self.label_count = self.get_labels()
 
     def get_labels(self):
         """ returns list of labels ordered by the node id's """
@@ -36,7 +37,7 @@ class Classification:
         node_list = lblmap.keys()
         node_list.sort()
         labels = [lblmap[vid] for vid in node_list]
-        return np.array(labels), len(set(labels))
+        return np.array(labels), node_list, len(set(labels))
 
     def get_multilabels(self, delim='\t'):
         """ returns the multibinarized object for multilabel datasets."""
@@ -54,7 +55,7 @@ class Classification:
         nlist = lblmap.keys()
         nlist.sort()
         labels = [lblmap[vid] for vid in nlist]
-        return self.binarize_labels(labels), len(unique_labels)
+        return self.binarize_labels(labels), nlist, len(unique_labels)
 
     def binarize_labels(self, labels, nclasses=None):
         """ returns the multilabelbinarizer object"""
@@ -65,23 +66,28 @@ class Classification:
         mlb = preprocessing.MultiLabelBinarizer(classes=range(nclasses))
         return mlb.fit_transform(labels)
 
-    def evaluate(self, model, label=False):
+    def evaluate(self, model, label=False, evaluation_scheme="tr"):
         embedding = 0
+        clf = self.get_classifier()
+
         if not label:
             embedding = self.get_embeddingDF(model)
 
-        clf = self.get_classifier()
-        for tr in self.TR:
-            print("TR ... ", tr)
-            if label:
-                model = paths.get_embedding_path_wl(self.dataset, tr)
-                if isinstance(model, str):
-                    embedding = self.get_embeddingDF(model)
+        if evaluation_scheme == "cv":
+            results = self.evaluate_cv(clf, embedding, 5)
+        elif evaluation_scheme == "tr" or label:
+            results = defaultdict(list)
+            for tr in self.TR:
+                print("TR ... ", tr)
+                if label:
+                    model = paths.get_embedding_path_wl(self.dataset, tr)
+                    if isinstance(model, str):
+                        embedding = self.get_embeddingDF(model)
+                results.update(self.evaluate_tr(clf, embedding, tr))
 
-            self.evaluate_tr(clf, embedding, tr)
         print("Training Finished")
 
-        df = pd.DataFrame(self.output)
+        df = pd.DataFrame(results)
         return df.groupby(axis=0, by="TR").mean()
 
     def get_embeddingDF(self, fname):
@@ -111,6 +117,32 @@ class Classification:
                 self.output["auc"].append(roc_auc_score(Y_test, probs[:, 1]))
             else:
                 self.output["auc"].append(0)
+        return self.output
+
+    def evaluate_cv(self, clf, embedding, n_splits):
+        """Do a repeated stratified cross validation.
+        :param clf: Classifier object.
+        :param embedding: The feature matrix.
+        :param n_splits: Number of folds.
+        :return: Dictionary containing numerical results of the classification.
+        """
+        embedding  = embedding[self.label_ind, :]
+        results = defaultdict(list)
+        for i in range(10):
+            rskf = StratifiedKFold(n_splits=n_splits, shuffle=True)
+            for train_idx, test_idx in rskf.split(embedding, self.labels):
+                X_train, X_test, Y_train, Y_test = self._get_split(embedding, test_idx, train_idx)
+                pred, probs = self.get_predictions(clf, X_train, X_test, Y_train, Y_test)
+                results["TR"].append(n_splits)
+                results["accuracy"].append(accuracy_score(Y_test, pred))
+                results["f1micro"].append(f1_score(Y_test, pred, average='micro'))
+                results["f1macro"].append(f1_score(Y_test, pred, average='macro'))
+                if self.label_count == 2:
+                    results["auc"].append(roc_auc_score(Y_test, probs[:, 1]))
+                else:
+                    results["auc"].append(0)
+        return results
+
 
     def _get_split(self, embedding, test_id, train_id):
         return embedding[train_id], embedding[test_id], self.labels[train_id], self.labels[test_id]
