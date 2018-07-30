30254933db0a1abc704cdfb2ecd534cbfb11e979
Özlem Muslu <ozlemmuslu@gmail.com>
2018-07-30 17:22:42 +0200

    Add setup file

---
+++ src/GAT2VEC/__init__.py
@@ -0,0 +1,14 @@
+
+"""Tool for analyzing gene expression using a protein protein interaction network."""
+
+__version__ = ''
+
+__title__ = 'GAT2VEC'
+__description__ = ''
+__url__ = ''
+
+__author__ = ''
+__email__ = ''
+
+__license__ = ''
+__copyright__ = ''
